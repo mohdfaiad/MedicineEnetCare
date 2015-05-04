70f7672c5e3b542f8cbf4d2163705bef63eca25c
benrogers1967 <benrogers1967@yahoo.com.au>
2015-05-04 22:19:45 +1000

    Add MVC Site map provider for menus

---
+++ ENetCareMVC.Web/Controllers/PackageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENetCareMVC.Web.Controllers
+{
+    public class PackageController : Controller
+    {
+        // GET: Package
+        public ActionResult RegisterPackage()
+        {
+            return View();
+        }
+
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        public ActionResult Receive()
+        {
+            return View();
+        }
+
+        public ActionResult Distribute()
+        {
+            return View();
+        }
+
+        public ActionResult Discard()
+        {
+            return View();
+        }
+
+        public ActionResult Audit()
+        {
+            return View();
+        }
+    }
+}
