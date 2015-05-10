fe005c6e3d044234312c1e9d5fb706bb20c15140
benrogers1967 <benrogers1967@yahoo.com.au>
2015-05-10 19:40:44 +1000

    Add Reconciled Package was missed last time

---
+++ ENetCareMVC.Repository/Data/ReconciledPackage.cs
@@ -0,0 +1,19 @@
+﻿using ENetCareMVC.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENetCareMVC.Repository.Data
+{
+    public class ReconciledPackage
+    {
+        public int PackageId { get; set; }
+        public string BarCode { get; set; }
+        public int CurrentLocationCentreId { get; set; }
+        public string CurrentLocationCentreName { get; set; }
+        public PackageStatus CurrentStatus { get; set; }
+        public PackageStatus NewStatus { get; set; }
+    }
+}
