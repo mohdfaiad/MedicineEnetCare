5eb433261357b99b1a201162c8499e24ba84f8db
benrogers1967 <benrogers1967@yahoo.com.au>
2015-05-12 22:14:47 +1000

    Add BarCodeListExtensions. Was missed

---
+++ ENetCareMVC.Repository/BarCodeListExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ENetCareMVC.Repository
+{
+    public static class BarCodeListExtension
+    {
+        public static XElement GetBarCodeXML(this List<string> barCodeList)
+        {
+            return new XElement("Root",
+                barCodeList.Select(b => new XElement("BarCode",
+                        new XAttribute("Text", b))));        
+        }
+    }
+}
