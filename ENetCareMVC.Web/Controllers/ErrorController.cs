5c798f6b63f2ca526ff2c24f7e33f4723018a688
benrogers1967 <benrogers1967@yahoo.com.au>
2015-05-24 15:52:34 +1000

    Setup custom errors and use employee service in layout

---
+++ ENetCareMVC.Web/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENetCareMVC.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Generic()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Generic(string action)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult MissingResource()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MissingResource(string action)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- ENetCareMVC.Web/Global.asax.cs
+++ ENetCareMVC.Web/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,46 @@ namespace ENetCareMVC.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = HttpContext.Current.Server.GetLastError();
+
+            //Handler HTTP errors
+            if (ex.GetType() == typeof(HttpException))
+            {
+                var httpCode = ((HttpException)ex).GetHttpCode();
+
+                // Do not log http 404 & 403 exception and let customErrors section handle it
+                if (httpCode == 404 || httpCode == 403)
+                    return;
+            }
+
+            // Get inner exception if there is one
+            if (ex is HttpUnhandledException && ex.InnerException != null)
+                ex = ex.InnerException;
+
+            var loggedInUser = "NONE";
+            if (User.Identity.IsAuthenticated)
+                loggedInUser = User.Identity.Name;
+
+            StringBuilder errorMsg = new StringBuilder();
+            errorMsg.AppendFormat("{0}: Unhandled exception occurred: {1}, URL: {2}, Username: {3}",
+                DateTime.Now,
+                ex.Message,
+                Request.RawUrl, loggedInUser);
+            errorMsg.AppendLine();
+            errorMsg.AppendFormat("Type: {0}", ex.GetType());
+            errorMsg.AppendLine();
+            errorMsg.AppendFormat("Source: {0}", ex.Source);
+            errorMsg.AppendLine();
+            errorMsg.AppendFormat("Stack Trace: {0}", ex.StackTrace);
+            errorMsg.AppendLine();
+
+            string errorLogPath = Server.MapPath("~/Log/error.log");
+
+            File.AppendAllText(errorLogPath, errorMsg.ToString());
         }
     }
 }
