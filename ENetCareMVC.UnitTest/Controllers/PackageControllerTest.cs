69da58ff754f52bc30a717cad7e7b27db106e690
benrogers1967 <benrogers1967@yahoo.com.au>
2015-05-24 23:10:29 +1000

    Add unit test for register on package controller

---
+++ ENetCareMVC.UnitTest/Controllers/PackageControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENetCareMVC.Web.Controllers;
+using System.Web.Mvc;
+using ENetCareMVC.Web.Models;
+
+namespace ENetCareMVC.UnitTest.Controllers
+{
+    [TestClass]
+    public class PackageControllerTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {           
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + "../../App_Data"));
+            // rest of initialize implementation ...
+        }
+        
+        [TestMethod]
+        public void TestRegister()
+        {
+            var controller = new PackageController();
+
+            var result = controller.Register() as ViewResult;
+
+            var model = result.Model as PackageRegisterViewModel;
+
+            Assert.AreEqual("Register", result.ViewName);
+            Assert.IsTrue(model.StandardPackageTypes.Any());
+        }
+    }
+}
