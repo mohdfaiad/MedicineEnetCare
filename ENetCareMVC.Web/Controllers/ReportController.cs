3c305fa6c6dd69cae80dd4d1ce8255d78e35c3bf
mrcactus <cactus0@yahoo.com>
2015-05-26 07:25:06 +1000

    Reports

--- ENetCareMVC.Web/Controllers/ReportController.cs
+++ ENetCareMVC.Web/Controllers/ReportController.cs
@@ -47,8 +47,8 @@ namespace ENetCareMVC.Web.Controllers
         public ActionResult DistributionCentreStock()
         {
             List<DistributionCentreStock> centreStockList = reportService.GetDistributionCentreStock();
-            //List<GlobalStock> mockedCentreStockList = getMockedCentreStock();
-            return View(centreStockList);
+            List<DistributionCentreStock> mockedCentreStockList = getMockedDistributionCentreStock();
+            return View(mockedCentreStockList);
         }
 
         public ActionResult Stocktaking()
@@ -75,21 +75,22 @@ namespace ENetCareMVC.Web.Controllers
         {
             //List<Package> packagesList = MockDataAccess.GetAllPackages();
             List<DistributionCentreLosses> lossesList = new List<DistributionCentreLosses>();
-
          
             DistributionCentreLosses l1 = new DistributionCentreLosses();
-            DistributionCentreLosses l2 = new DistributionCentreLosses();
-            DistributionCentreLosses l3 = new DistributionCentreLosses();
             l1.DistributionCenterName = "CentreA";
             l1.DistributionCentreId = 1;
             l1.LossRatioDenominator = 75;
             l1.LossRatioNumerator = 12;
             l1.TotalLossDiscardedValue = 445;
+
+            DistributionCentreLosses l2 = new DistributionCentreLosses();
             l2.DistributionCenterName = "CentreB";
             l2.DistributionCentreId = 2;
             l2.LossRatioDenominator = 275;
             l2.LossRatioNumerator = 18;
             l2.TotalLossDiscardedValue = 1445;
+
+            DistributionCentreLosses l3 = new DistributionCentreLosses();
             l3.DistributionCenterName = "CentreC";
             l3.DistributionCentreId = 3;
             l3.LossRatioDenominator = 175;
@@ -99,6 +100,37 @@ namespace ENetCareMVC.Web.Controllers
             lossesList.Add(l1); lossesList.Add(l2); lossesList.Add(l3);
             return lossesList;
             
+        }
+
+        public List<DistributionCentreStock> getMockedDistributionCentreStock()
+        {
+            List<DistributionCentreStock> stockList = new List<DistributionCentreStock>();
+            DistributionCentreStock s1 = new DistributionCentreStock();
+            s1.DistributionCenterName = "CentreA";
+            s1.DistributionCentreId = 1;
+            s1.CostPerPackage = 44;
+            s1.NumberOfPackages = 5;
+            s1.PackageTypeId = 4;
+            s1.TotalValue = 145;
+
+            DistributionCentreStock s2 = new DistributionCentreStock();
+            s2.DistributionCenterName = "CentreB";
+            s2.DistributionCentreId = 2;
+            s2.CostPerPackage = 34;
+            s2.NumberOfPackages = 15;
+            s2.PackageTypeId = 3;
+            s2.TotalValue = 245;
+
+            DistributionCentreStock s3 = new DistributionCentreStock();
+            s3.DistributionCenterName = "CentreC";
+            s3.DistributionCentreId = 3;
+            s3.CostPerPackage = 14;
+            s3.NumberOfPackages = 12;
+            s3.PackageTypeId = 1;
+            s3.TotalValue = 45;
+
+            stockList.Add(s1); stockList.Add(s2); stockList.Add(s3);
+            return stockList;
         }
 
         public ActionResult Index()
